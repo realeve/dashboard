d4f6e14935a14da25691c57638bfbe4d842c7b72
realeve <realeve@qq.com>
2020-01-23 14:35:08 +0800

    翻牌器完成

--- src/component/widget/FlipBoard/index.tsx
+++ src/component/widget/FlipBoard/index.tsx
@@ -1,35 +1,57 @@
-import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
-import React from 'react';
+// import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
+import React, { useEffect } from 'react';
 import styles from './index.less';
-import { useCountUp } from 'react-countup';
+import { useCountUp, CountUpProps } from 'react-countup';
+import classnames from 'classnames';
 import { thouandsNum } from '@/utils/lib';
+
+export interface ICountUp extends CountUpProps {
+  title: string;
+  value: number;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  decimals?: number;
+  className?: string;
+  style?: React.CSSProperties;
+  [key: string]: any;
+}
 
 export default ({
   title = '',
   value,
-  prefix = <ArrowUpOutlined />,
-  suffix = '',
-  precision = 0,
+  prefix = null,
+  suffix = null,
+  decimals = 0,
+  className,
+  style,
+  duration = 1.5,
   ...props
-}) => {
-  const { countUp } = useCountUp({ end: Number(value) });
+}: ICountUp) => {
+  const { countUp, update } = useCountUp({ end: Number(value), duration, decimals, ...props });
+  useEffect(() => {
+    update(Number(value));
+  }, [value]);
+
   return (
-    <div className={styles.flipBoard}>
-      {prefix}
-      {thouandsNum(countUp as number, precision)
-        .split('')
-        .map((item, idx) => (
-          <div
-            className={styles.value}
-            style={
-              [',', '.'].includes(item) ? { backgroundColor: 'transparent', padding: 0 } : null
-            }
-            key={idx}
-          >
-            {item}
-          </div>
-        ))}
-      <span>{suffix}</span>
+    <div className={classnames(styles.flipBoard, className)} style={style}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.counter}>
+        {prefix}
+        {thouandsNum(countUp as number, decimals)
+          .split('')
+          .map((item, idx) => (
+            <div
+              className={styles.value}
+              style={
+                [',', '.'].includes(item) ? { backgroundColor: 'transparent', padding: 0 } : null
+              }
+              key={idx}
+            >
+              {item}
+            </div>
+          ))}
+        <span>{suffix}</span>
+      </div>
     </div>
   );
 };
