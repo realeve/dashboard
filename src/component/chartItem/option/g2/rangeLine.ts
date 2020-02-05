795e938116223ac7799647f3b65a88840dda23f2
realeve <realeve@qq.com>
2020-02-06 00:14:45 +0800

    样式调整

--- src/component/chartItem/option/g2/rangeLine.ts
+++ src/component/chartItem/option/g2/rangeLine.ts
@@ -1,6 +1,5 @@
 import { textColor } from './../index';
 import * as R from 'ramda';
-import { handleAxisStyle } from './waterfall';
 
 const transform = ({ data, x, y, y2, y3 }) =>
   R.map(item => ({
@@ -10,51 +9,16 @@ const transform = ({ data, x, y, y2, y3 }) =>
   }))(data);
 
 export const onMount = (
-  { data: val, rangeChart = 'area', header, x = 0, y = 1, y2 = 2, y3 = 3 },
+  { data: val, rangeChart = 'area', coord = null, header, x = 0, y = 1, y2 = 2, y3 = 3 },
   chart,
 ) => {
   let rangeChartType = rangeChart === 'bar' ? 'interval' : 'area';
+  let showAverage = !R.isNil(header[y3]);
   chart.scale({
     y: {
       sync: true,
     },
   });
-
-  let data = transform({ data: val, x, y, y2, y3 });
-  let data2 = data.map(item => ({ x: item.x, y: item.y2 }));
-  let view1 = chart.view(),
-    view2 = chart.view();
-  view1.source(data), view2.source(data2);
-  view1[rangeChartType]()
-    .position('x*y')
-    .color('#1890ff')
-    .shape('smooth')
-    .opacity(0.1)
-    .tooltip('y', y => {
-      return {
-        name: '数据区间',
-        value: y[0] + ' 至 ' + y[1],
-      };
-    });
-
-  view2
-    .line()
-    .position('x*y')
-    .shape('smooth')
-    .tooltip('y', y => {
-      return {
-        name: '均值',
-        value: y,
-      };
-    });
-  view2
-    .point()
-    .position('x*y')
-    .label('y', {
-      textStyle: {
-        fill: textColor,
-      },
-    });
 
   let fieldColor = {
     label: {
@@ -74,37 +38,92 @@ export const onMount = (
     ...fieldColor,
   };
 
+  let data = transform({ data: val, x, y, y2, y3 });
+  let view1 = chart.view();
+  view1.source(data);
+
+  view1[rangeChartType]()
+    .position('x*y')
+    .color('#1890ff')
+    .shape('smooth')
+    .opacity(showAverage ? 0.2 : 0.8)
+    .tooltip('y', y => {
+      return {
+        name: '数据区间',
+        value: y[0] + ' 至 ' + y[1],
+      };
+    });
+
   view1.axis('x', {
     tickLine: {
       visible: false,
     },
     ...fieldColor,
   });
-  view2.axis('x', {
-    tickLine: {
-      visible: false,
-    },
-    ...fieldColor,
-  });
   view1.axis('y', yStyle);
-  view2.axis('y', yStyle);
 
-  chart.legend({
-    custom: true,
-    clickable: false,
-    items: [
-      { value: '极值', marker: { symbol: 'circle', fill: '#34a4', radius: 5 } },
-      { value: '均值', marker: { symbol: 'circle', fill: '#1890FF', radius: 5 } },
-    ],
-  });
+  if (showAverage) {
+    let data2 = data.map(item => ({ x: item.x, y: item.y2 }));
+    let view2 = chart.view();
+    view2.source(data2);
+    view2
+      .line()
+      .position('x*y')
+      .shape('smooth')
+      .tooltip('y', y => {
+        return {
+          name: '均值',
+          value: y,
+        };
+      });
+    view2
+      .point()
+      .position('x*y')
+      .label('y', {
+        textStyle: {
+          fill: textColor,
+        },
+      });
+    view2.axis('x', {
+      tickLine: {
+        visible: false,
+      },
+      ...fieldColor,
+    });
+    view2.axis('y', yStyle);
+
+    chart.legend({
+      custom: true,
+      clickable: false,
+      items: [
+        { value: '极值', marker: { symbol: 'circle', fill: '#34a4', radius: 5 } },
+        { value: '均值', marker: { symbol: 'circle', fill: '#1890FF', radius: 5 } },
+      ],
+    });
+
+    if (coord === 'polar') {
+      view1.coord(coord, {
+        innerRadius: 0.5,
+      });
+
+      view2.coord(coord, {
+        innerRadius: 0.5,
+      });
+    }
+  }
+
   chart.render();
 
   const onChange = ({ data: val, view1, view2 }) => {
     let data = transform({ data: val, x, y, y2, y3 });
-    let data2 = data.map(item => ({ x: item.x, y: item.y2 }));
     view1.changeData(data);
-    view2.changeData(data2);
+
+    if (showAverage) {
+      let data2 = data.map(item => ({ x: item.x, y: item.y2 }));
+      view2.changeData(data2);
+    }
   };
+
   return onChange;
 };
 
