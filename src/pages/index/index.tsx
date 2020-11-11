d541d63532de89342f43543a5003399798636adc
realeve <realeve@qq.com>
2020-11-11 10:38:45 +0800

    支持对页面尺寸的自动缩放

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,34 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'dva';
 import DNA from './dna';
 import styles from './index.less';
-
-import { getConfig, getStyle } from './lib';
+import { getConfig, getStyle, getAutoSizeStyle, getBackground } from './lib';
 import { GROUP_COMPONENT_KEY } from '@/models/common';
 import { ChartItem } from '@/pages/config/canvas/chartItem';
-import { getDashboardStyle } from '@/component/Editor/Editor';
 
+// DEMO:  http://localhost:8000/?id=/data/01.json&autoresize=1
 const Index = ({ location }) => {
   const [config, setConfig] = useState(null);
   useEffect(() => {
     setConfig(null);
     getConfig(location.query.id).then(setConfig);
   }, [location.query.id]);
-
   if (!config) {
     return <DNA />;
   }
-  let { page, panel } = config;
-
+  let { page, panel } = config,
+    autoSize = location?.query?.autoresize == '1';
   return (
-    <div className={styles.dashboard} style={getDashboardStyle(page)}>
-      {panel.map(
-        (item) =>
-          item.key !== GROUP_COMPONENT_KEY && (
-            <div style={getStyle({ style: item.style, page })} key={item.id}>
-              <ChartItem config={item} page={page} />
-            </div>
-          ),
-      )}
+    <div className={styles.dashboard} style={getBackground(page)}>
+      <div style={getAutoSizeStyle(page, autoSize)}>
+        {panel.map(
+          (item) =>
+            item.key !== GROUP_COMPONENT_KEY && (
+              <div style={getStyle({ style: item.style, autoSize, page })} key={item.id}>
+                <ChartItem config={item} page={page} />
+              </div>
+            ),
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/index/lib.tsx
+++ src/pages/index/lib.tsx
@@ -1,4 +1,6 @@
 import { axios } from '@/utils/axios';
+import { getDashboardStyle } from '@/component/Editor/Editor';
+
 export const getConfig = async (url) => {
   if (url) {
     let option: { url: string; [key: string]: string } = { url };
@@ -18,20 +20,32 @@ export const getConfig = async (url) => {
 };
 
 /**
+ * 将style中   233px 转换为 数值型 233
+ * @param style 样式内容
+ * @param unit 单位
+ */
+const parseStyle = (style: string, unit = 'px') => {
+  let reg = RegExp(unit, 'g');
+  return Number(style.replace(reg, ''));
+};
+
+/**
  * 计算主页渲染的样式
  * @param style 在config中配置的样式，计算为主页渲染所需的样式
+ * @param page 页面宽高配置
+ * @param autoResize 是否启动自动调整页面大小
  */
-export const getStyle = ({ style, page }) => {
+export const getStyle = ({ style, page, autoSize = false }) => {
   if (!style) {
     return {};
   }
 
-  // TODO 1.将transform中的translate部分转换为left和top
+  // STEP 1.将transform中的translate部分转换为left和top
   // 将transform中的内容优化
   let { left = '0', top = '0', transform } = style;
 
-  left = Number(left.replace('px', ''));
-  top = Number(top.replace('px', ''));
+  left = parseStyle(left);
+  top = parseStyle(top);
 
   let rotate = '',
     scale = '',
@@ -47,17 +61,50 @@ export const getStyle = ({ style, page }) => {
       left += translate[0];
       top += translate[1];
     }
-    rotate = Number(transform.rotate.replace('deg', '')) > 0 ? `rotate(${transform?.rotate}) ` : '';
+    rotate = parseStyle(transform.rotate, 'deg') > 0 ? `rotate(${transform?.rotate}) ` : '';
     scale = transform.scale == '1,1' ? '' : `scale(${transform?.scale})`;
     _transform = (rotate + scale).length > 0 ? { transform: rotate + scale } : {};
   }
 
-  // TODO 2.根据 page 的宽高和当前 window 的宽高做尺寸转换
   return {
     ...style,
     left,
     top,
     ..._transform,
     position: 'absolute',
+  };
+};
+
+// 获取缩放系数
+let getScale = (page) => {
+  let { width, height } = page;
+  let { innerWidth, innerHeight } = window;
+  let scaleX = innerWidth / width,
+    scaleY = innerHeight / height;
+  return { scaleX, scaleY };
+};
+
+// 获取背景
+export let getBackground = (page) => {
+  let { width, height, ...background } = getDashboardStyle(page);
+  return background;
+};
+/**
+ * 根据 page 的宽高和当前 window 的宽高做尺寸转换
+ * @param page 页面基础配置
+ * @param autosize 是否自动调整大小
+ */
+export const getAutoSizeStyle = (page, autosize = false) => {
+  let { width: styleWidth, height: styleHeight } = getDashboardStyle(page);
+  let { width, height } = page;
+  let { innerWidth, innerHeight } = window;
+  let { scaleX, scaleY } = getScale(page);
+
+  let scale = ` scale(${scaleX},${scaleY})`;
+  let translate = `translate(${(innerWidth - width) / 2}px,${(innerHeight - height) / 2}px)`;
+  return {
+    width: styleWidth,
+    height: styleHeight,
+    transform: translate + scale,
   };
 };
