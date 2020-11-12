535a8557f115e4f05eecded8f9fc5e6fcb96eed3
realeve <realeve@qq.com>
2020-11-12 17:33:09 +0800

    增加localforage存储设置

--- src/services/db.tsx
+++ src/services/db.tsx
@@ -1,5 +1,11 @@
 import localforage from 'localforage';
 
+localforage.config({
+  name: 'dashboard',
+  storeName: 'tbl_dashboard', // Should be alphanumeric, with underscores.
+  description: 'dashboard 离线数据',
+});
+
 export const loadPanel: (key?: string) => Promise<unknown> = (key = 'panel') =>
   localforage.getItem(key).then((res) => res || []);
 export const savePanel = (key = 'panel') => (panel) => localforage.setItem(key, panel);
