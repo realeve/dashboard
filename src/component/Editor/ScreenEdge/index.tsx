179c0a92aa66cf5d4738913bc61386a63a7f57bb
realeve <realeve@qq.com>
2020-11-13 19:41:38 +0800

    :art: 优化屏幕边缘代码

--- src/component/Editor/ScreenEdge/index.tsx
+++ src/component/Editor/ScreenEdge/index.tsx
@@ -1,63 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { IPage } from '@/models/common';
 import styles from './index.less';
 
-let getEdgeArr = (width: number, screenNum: number) => {
-  if (screenNum < 2) {
+let getEdgeArr = (width: string | number, screenNum: string | number) => {
+  if (Number(screenNum) < 2) {
     return [];
   }
-  let step = width / screenNum;
-  let arr = [];
-  for (let i = 1; step * i < width; i++) {
+  let step = Number(width) / Number(screenNum),
+    arr = [];
+  for (let i = 1; step * i < Number(width); i++) {
     arr.push(i * step);
   }
   return arr;
 };
 
-const getEdgeLine = (page: {
-  screen_x: string | number;
-  screen_y: string | number;
-  width: string | number;
-  height: string | number;
-}) => {
-  let screen_x = Number(page.screen_x),
-    screen_y = Number(page.screen_y),
-    width = Number(page.width),
-    height = Number(page.height);
-
-  return {
-    vertical: getEdgeArr(width, screen_x),
-    horizontal: getEdgeArr(height, screen_y),
-  };
-};
-
 const EdgeLine = ({ direction = 'horizontal', data, screen_edge_width, showEdgeTag = true }) => {
   let isHorizontal = direction == 'horizontal';
-  let style = !isHorizontal
-    ? {
-        width: screen_edge_width,
-        height: '100%',
-      }
-    : {
-        height: screen_edge_width,
-        width: '100%',
-      };
   return (
-    <div
-      className={`datav-manager datav-${direction} scena-guides scena-${direction}`}
-      style={{ width: '100%', height: '100%' }}
-    >
+    <div className={`scena-guides scena-${direction}`} style={{ width: '100%', height: '100%' }}>
       <div className="datav-guides" style={{ transform: 'translateX(-10px)', height: '100%' }}>
         {data.map((item) => (
           <div
             className={styles.screenEdge + ` datav-${direction}`}
             key={item + 'px'}
             style={{
-              transform:
-                direction === 'vertical'
-                  ? `translate(${item}px,-30px)`
-                  : `translate(-20px,${item}px)`,
-              ...style,
+              transform: !isHorizontal
+                ? `translate(${item}px,-30px)`
+                : `translate(-20px,${item}px)`,
+              [isHorizontal ? 'height' : 'width']: screen_edge_width,
+              [!isHorizontal ? 'height' : 'width']: '100%',
             }}
           >
             {showEdgeTag && <span>{item}</span>}
@@ -68,34 +39,20 @@ const EdgeLine = ({ direction = 'horizontal', data, screen_edge_width, showEdgeT
   );
 };
 
-export default ({
-  page: { screen_x, screen_y, screen_edge_width, width, height, showEdgeTag },
-}: {
-  page: IPage;
-}) => {
-  const [state, setState] = useState({
-    vertical: [],
-    horizontal: [],
-  });
-
-  useEffect(() => {
-    let arr = getEdgeLine({ screen_x, screen_y, width, height });
-    setState(arr);
-  }, [JSON.stringify({ screen_x, screen_y, screen_edge_width, width, height })]);
-
+export default ({ page }: { page: IPage }) => {
   return (
     <>
       <EdgeLine
-        data={state.vertical}
-        showEdgeTag={showEdgeTag}
-        screen_edge_width={screen_edge_width}
+        data={getEdgeArr(page.width, page.screen_x)}
+        showEdgeTag={page.showEdgeTag}
+        screen_edge_width={page.screen_edge_width}
         direction="vertical"
       />
       <EdgeLine
-        data={state.horizontal}
-        screen_edge_width={screen_edge_width}
+        data={getEdgeArr(page.height, page.screen_y)}
+        screen_edge_width={page.screen_edge_width}
         direction="horizontal"
-        showEdgeTag={showEdgeTag}
+        showEdgeTag={page.showEdgeTag}
       />
     </>
   );
