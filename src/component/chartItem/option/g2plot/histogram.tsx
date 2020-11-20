73e85ceafb40991fa96e76efbd8b1341717fed6c
realeve <realeve@qq.com>
2020-11-20 23:46:36 +0800

    更新直方图，支持分组直方图

--- src/component/chartItem/option/g2plot/histogram.tsx
+++ src/component/chartItem/option/g2plot/histogram.tsx
@@ -1,64 +1,66 @@
 import { IChartMock, IApiConfig, IG2PlotProps } from '@/component/chartItem/interface';
+import * as lib from '@/component/chartItem/option/lib';
+import { getTheme } from './lib';
 
 export let mock: IChartMock = {
   data: [
-    { value: 1.2 },
-    { value: 3.4 },
-    { value: 3.7 },
-    { value: 4.3 },
-    { value: 5.2 },
-    { value: 5.8 },
-    { value: 6.1 },
-    { value: 6.5 },
-    { value: 6.8 },
-    { value: 7.1 },
-    { value: 7.3 },
-    { value: 7.7 },
-    { value: 8.3 },
-    { value: 8.6 },
-    { value: 8.8 },
-    { value: 9.1 },
-    { value: 9.2 },
-    { value: 9.4 },
-    { value: 9.5 },
-    { value: 9.7 },
-    { value: 10.5 },
-    { value: 10.7 },
-    { value: 10.8 },
-    { value: 11.0 },
-    { value: 11.0 },
-    { value: 11.1 },
-    { value: 11.2 },
-    { value: 11.3 },
-    { value: 11.4 },
-    { value: 11.4 },
-    { value: 11.7 },
-    { value: 12.0 },
-    { value: 12.9 },
-    { value: 12.9 },
-    { value: 13.3 },
-    { value: 13.7 },
-    { value: 13.8 },
-    { value: 13.9 },
-    { value: 14.0 },
-    { value: 14.2 },
-    { value: 14.5 },
-    { value: 15 },
-    { value: 15.2 },
-    { value: 15.6 },
-    { value: 16.0 },
-    { value: 16.3 },
-    { value: 17.3 },
-    { value: 17.5 },
-    { value: 17.9 },
-    { value: 18.0 },
-    { value: 18.0 },
-    { value: 20.6 },
-    { value: 21 },
-    { value: 23.4 },
+    { value: 1.2, type: 'a' },
+    { value: 3.4, type: 'b' },
+    { value: 3.7, type: 'a' },
+    { value: 4.3, type: 'b' },
+    { value: 5.2, type: 'a' },
+    { value: 5.8, type: 'b' },
+    { value: 6.1, type: 'a' },
+    { value: 6.5, type: 'b' },
+    { value: 6.8, type: 'a' },
+    { value: 7.1, type: 'b' },
+    { value: 7.3, type: 'a' },
+    { value: 7.7, type: 'b' },
+    { value: 8.3, type: 'a' },
+    { value: 8.6, type: 'b' },
+    { value: 8.8, type: 'a' },
+    { value: 9.1, type: 'b' },
+    { value: 9.2, type: 'a' },
+    { value: 9.4, type: 'b' },
+    { value: 9.5, type: 'a' },
+    { value: 9.7, type: 'b' },
+    { value: 10.5, type: 'a' },
+    { value: 10.7, type: 'b' },
+    { value: 10.8, type: 'a' },
+    { value: 11, type: 'b' },
+    { value: 11, type: 'a' },
+    { value: 11.1, type: 'b' },
+    { value: 11.2, type: 'a' },
+    { value: 11.3, type: 'b' },
+    { value: 11.4, type: 'a' },
+    { value: 11.4, type: 'b' },
+    { value: 11.7, type: 'a' },
+    { value: 12, type: 'b' },
+    { value: 12.9, type: 'a' },
+    { value: 12.9, type: 'b' },
+    { value: 13.3, type: 'a' },
+    { value: 13.7, type: 'b' },
+    { value: 13.8, type: 'a' },
+    { value: 13.9, type: 'b' },
+    { value: 14, type: 'a' },
+    { value: 14.2, type: 'b' },
+    { value: 14.5, type: 'a' },
+    { value: 15, type: 'b' },
+    { value: 15.2, type: 'a' },
+    { value: 15.6, type: 'b' },
+    { value: 16, type: 'a' },
+    { value: 16.3, type: 'b' },
+    { value: 17.3, type: 'a' },
+    { value: 17.5, type: 'b' },
+    { value: 17.9, type: 'a' },
+    { value: 18, type: 'b' },
+    { value: 18, type: 'a' },
+    { value: 20.6, type: 'b' },
+    { value: 21, type: 'a' },
+    { value: 23.4, type: 'b' },
   ],
   title: '拆线柱图_MOCK数据',
-  header: ['value'],
+  header: ['value', 'type'],
   rows: 36,
   hash: 'mockdata',
 };
@@ -109,6 +111,8 @@ export const config = [
     min: -100,
     max: 100,
   },
+  lib.getAntThemePanel(),
+  ...lib.getLegendConfig(),
 ];
 
 export const apiConfig: IApiConfig = {
@@ -121,6 +125,12 @@ export const apiConfig: IApiConfig = {
       key: 'x',
       title: 'x 字段',
       defaultValue: 0,
+      min: 0,
+    },
+    {
+      key: 'legend',
+      title: 'legend 字段',
+      defaultValue: 1,
       min: 0,
     },
   ],
@@ -137,11 +147,17 @@ interface IG2Plot extends IG2PlotProps {
 export default ({
   data: { data, header },
   x = 0,
+  legend = 1,
   binNumber = 5,
   binWidth = 5,
   color = '#096dd9',
   renderer = 'svg',
   offsetX = -56,
+  theme = 18,
+  legendShow = true,
+  legendAlign,
+  legendPosition,
+  legendOrient,
 }: IG2Plot) => {
   let binConfig =
     binWidth == 0
@@ -151,25 +167,49 @@ export default ({
       : {
           binWidth,
         };
+  let noLegend = typeof legend == 'undefined' || header.length == 1;
+  let stackCfg = noLegend
+    ? {
+        label: {
+          position: 'top',
+          offsetX,
+        },
+        color,
+      }
+    : {
+        stackField: header[legend],
+        label: {
+          position: 'middle',
+          offsetX,
+        },
+      };
+
   return {
     chartType: 'histogram',
     renderer,
     data,
-    color,
+    ...getTheme(theme),
+    ...lib.getG2LegendOption({ legendShow, legendAlign, legendPosition, legendOrient }),
     binField: header[x],
     ...binConfig,
-    label: {
-      position: 'top',
-      offsetX,
-    },
+    ...stackCfg,
     tooltip: {
-      showTitle: false,
-      formatter: ({ range, count }) => {
-        return {
-          name: range.map((item) => item.toFixed(1)).join(' ~ '),
-          value: count,
-        };
+      showMakers: true,
+      formatter: ({ range, count, ...props }) => {
+        let name = range.map((item) => item.toFixed(1)).join(' ~ ');
+        return noLegend
+          ? {
+              name,
+              value: count,
+            }
+          : {
+              name: props[header[legend]] + ` (${name}) `,
+              value: count,
+            };
       },
+    },
+    yAxis: {
+      grid: null,
     },
   };
 };
