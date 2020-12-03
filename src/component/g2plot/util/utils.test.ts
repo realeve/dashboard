75dfaf24310116839669f254ef39e957366fed03
realeve <realeve@qq.com>
2020-12-03 21:51:25 +0800

    update unit test

--- src/component/g2plot/util/utils.test.ts
+++ src/component/g2plot/util/utils.test.ts
@@ -2,6 +2,7 @@ import * as lib from './utils';
 // umi test ./src/component/g2plot/util/utils.test.ts
 test('g2 plot通用功能', () => {
   expect(lib.camelCase('helloKitty')).toBe('HelloKitty');
+  expect(lib.camelCase('hello_kitty')).toBe('HelloKitty');
   expect(lib.checkChanged({ a: 2 }, { a: 3 })).toBeFalsy();
 
   expect(lib.sameArray([2, 3], [2, 3])).toBeTruthy();
