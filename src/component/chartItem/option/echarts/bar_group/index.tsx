adecc9c1ba5edfe31372a65820ffb341cbfc226f
WBO <pepperbeibei@gmail.com>
2021-01-21 09:51:17 +0800

    优化bar_group的显示

--- src/component/chartItem/option/echarts/bar_group/index.tsx
+++ src/component/chartItem/option/echarts/bar_group/index.tsx
@@ -2,12 +2,13 @@ import * as R from 'ramda';
 
 export { mock, apiConfig, config, defaultOption } from './mock';
 
-export default ({ data }) => {
-  const planName = '计划量';
+export default ({ data, product = '9997T' }: { data: any; product: string }) => {
   const barWidth = 30;
-  const product: string = '9997T';
+  const planName = '计划量';
   const whitename = '白纸投入';
   const whitepaper = '白纸';
+  const percent = 0.3; // 较短的bar不显示label
+  let planDataMin = 0;
 
   const seriesStyle = {
     type: 'bar',
@@ -15,7 +16,7 @@ export default ({ data }) => {
     label: {
       show: true,
       formatter(e) {
-        return `${e.name}: ${e.value}`;
+        return planDataMin * percent - e.value > 0 ? '' : `${e.name}: ${e.value}`;
       },
       position: 'insideRight',
     },
@@ -25,109 +26,95 @@ export default ({ data }) => {
   const handleXData = ({ data: _data, header: _header }): string[] => {
     const prodata = R.filter(R.propEq(_header[0], product))(_data as any[]);
     const allProcessData = R.reject((item: any) => R.equals(item[_header[2]], planName))(prodata);
-
+    // console.log('1');
     return R.uniq(R.pluck(_header[1])(allProcessData));
   };
 
   const xAxisData = handleXData(data);
-  console.log('xaxis', xAxisData);
 
-  const handleData = ({ data: _data, header: _header }) =>
-    // { legend: _legend, x: _x, y: _y }: { legend?: number | string; x: number | string; y: number | string },
-    {
-      const prodata = R.filter(R.propEq(_header[0], product))(_data as any[]);
-      let series = [];
-
-      // if (typeof legend === 'undefined') {
-      // } else {
-      const whitePut = R.project(
-        [_header[2], _header[3]],
-        R.filter(R.propEq(_header[1], whitepaper))(prodata),
-      );
-      let whiteData: any[] = [];
-      whiteData.push({ name: whitePut[0][_header[2]], value: Number(whitePut[0][_header[3]]) });
-      whiteData = R.concat(whiteData, R.repeat('-', xAxisData.length - 1));
-
-      // console.log('whiteData', whiteData);
-      series.push({
-        type: 'bar',
-        stack: 'total',
-        label: {
-          show: true,
+  const handleData = ({ data: _data, header: _header }) => {
+    const prodata = R.filter(R.propEq(_header[0], product))(_data as any[]);
+    let series = [];
+    const whitePut = R.project(
+      [_header[2], _header[3]],
+      R.filter(R.propEq(_header[1], whitepaper))(prodata),
+    );
+    let whiteData: any[] = [
+      { name: whitePut[0][_header[2]], value: Number(whitePut[0][_header[3]]) },
+    ];
+    whiteData = R.concat(whiteData, R.repeat('-', xAxisData.length - 1));
+    series.push({
+      type: 'bar',
+      stack: 'total',
+      label: {
+        show: true,
+        formatter(e) {
+          return `${e.name}: ${e.value}`;
         },
-        data: whiteData,
-        barWidth,
-      });
+      },
+      data: whiteData,
+      barWidth,
+    });
 
-      const allProcessData = R.reject((item: any) => R.equals(item[_header[2]], planName))(prodata);
-      const allProcessGroup = R.groupBy((c: any) => c[_header[1]])(allProcessData);
-      const processData = R.reject(
-        (item: any) =>
-          R.equals(item[_header[2]], planName) || R.equals(item[_header[2]], whitename),
-      )(prodata);
-      const processGroup = R.groupBy((c: any) => c[_header[1]])(processData);
-      const yAxisGroup: number[] = [];
-      R.mapObjIndexed((value: any[]) => {
-        yAxisGroup.push(value.length);
-        // yAxisData.push(value);
-      })(allProcessGroup);
-      const yAxisGroupMax = Math.max(...yAxisGroup);
-      // let yAxisData1 = new Array(2);
-      let yAxisData: any[] = [];
-      R.forEach((element) => {
-        element = new Array(xAxisData.length).fill('-');
-        yAxisData.push({ data: element });
-      })(new Array(yAxisGroupMax));
+    const yAxisData: any[] = [];
+    const allProcessData = R.reject((item: any) => R.equals(item[_header[2]], planName))(prodata);
+    const allProcessGroup = R.groupBy((c: any) => c[_header[1]])(allProcessData);
+    const processData = R.reject(
+      (item: any) => R.equals(item[_header[2]], planName) || R.equals(item[_header[2]], whitename),
+    )(prodata);
+    const yAxisGroup: number[] = [];
+    R.mapObjIndexed((value: any[]) => {
+      yAxisGroup.push(value.length);
+    })(allProcessGroup);
+    R.forEach((element) => {
+      element = new Array(xAxisData.length).fill('-');
+      yAxisData.push({ data: element });
+    })(new Array(Math.max(...yAxisGroup)));
 
-      R.mapObjIndexed((value: any, key: string) => {
-        const no = R.indexOf(key)(xAxisData);
-        for (let loop = 0; loop < value.length; loop++) {
-          yAxisData[loop]['data'][no] = {
-            name: value[loop][_header[2]],
-            value: value[loop][_header[3]],
-          };
-        }
-      })(processGroup);
+    const processGroup = R.groupBy((c: any) => c[_header[1]])(processData);
+    R.mapObjIndexed((value: any, key: string) => {
+      const no = R.indexOf(key)(xAxisData);
+      for (let loop = 0; loop < value.length; loop++) {
+        yAxisData[loop].data[no] = {
+          name: value[loop][_header[2]],
+          value: Number(value[loop][_header[3]]),
+        };
+      }
+    })(processGroup);
 
-      yAxisData = R.map((item: {}) => {
-        // console.log("item", item);
-        return R.mergeLeft(seriesStyle, item);
-      })(yAxisData);
-      // console.log("yAxisData", yAxisData);
-      series = R.concat(series, yAxisData);
+    series = R.concat(series, R.map((item: any) => R.mergeLeft(seriesStyle, item))(yAxisData));
 
-      const planData: number[] = R.pluck(_header[3])(
-        R.filter(R.propEq(_header[2], planName))(prodata),
-      ).map(Number);
-      // console.log('planData', planData);
-      series.push({
-        name: planName,
-        type: 'line',
-        data: planData,
-        lineStyle: {
-          width: 0,
+    const planData: number[] = R.pluck(_header[3])(
+      R.filter(R.propEq(_header[2], planName))(prodata),
+    ).map(Number);
+    planDataMin = Math.min(...planData);
+    series.push({
+      name: planName,
+      type: 'line',
+      data: planData,
+      lineStyle: {
+        width: 0,
+      },
+      color: '#0ef',
+      symbolSize: [2, 16 + barWidth],
+      symbol: 'rect',
+      label: {
+        show: true,
+        position: 'center',
+        rotate: 90,
+        offset: [-4 - barWidth, 6],
+        color: '#fff',
+        formatter(e) {
+          if (e.dataIndex === xAxisData.length - 1) {
+            return `${e.value}(${planName})`;
+          }
+          return e.value;
         },
-        color: '#0ef',
-        symbolSize: [2, 16 + barWidth],
-        symbol: 'rect',
-        label: {
-          show: true,
-          position: 'center',
-          rotate: 90,
-          offset: [-4 - barWidth, 6],
-          color: '#fff',
-          formatter(e) {
-            if (e.dataIndex === xAxisData.length - 1) {
-              return `${e.value}(${planName})`;
-            }
-            return e.value;
-          },
-        },
-      });
-      // }
+      },
+    });
 
-      return series;
-    };
+    return series;
+  };
 
   const series = handleData(data);
 
